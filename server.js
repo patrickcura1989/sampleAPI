b2c2160a57ecba115cdf7d20d87e0b1fc447167e
LEGION-PC <patrick_cura1989@yahoo.com>
2023-06-20 00:25:49 +1200

    edit header

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ let fetch = require('node-fetch');
 
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
 
